f3aa08267fca4a14a21028b6a0c28ae0aa5a4b93
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-15 09:23:05 +0400

    context

--- src/context/Context.js
+++ src/context/Context.js
@@ -4,27 +4,39 @@ import { auth } from "../firebase";
 
 const Context = React.createContext();
 
-export const DataLayerContext = () => useContext(Context);
-
-export const Provider = ({ children }) => {
+export const DataProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      setUser(user);
-      setLoading(false);
-      navigate("/chats");
-    });
+    let isRender = false;
+
+    const handle = async () => {
+      await setTimeout(1000);
+
+      if (!isRender) {
+        auth.onAuthStateChanged((user) => {
+          setUser(user);
+          setLoading(false);
+          if (user) navigate("/home");
+        });
+      }
+    };
+
+    handle();
+
+    return () => {
+      isRender = true;
+    };
   }, [user, navigate]);
 
-  const value = { user }
+  const value = { user };
 
   return (
-    <DataLayerContext.Provider value={value}>
-        {!loading && children}
-    </DataLayerContext.Provider>
-  )
+    <Context.Provider value={value}>{!loading && children}</Context.Provider>
+  );
 };
+
+export const DataLayerContext = () => useContext(Context);
