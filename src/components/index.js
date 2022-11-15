5fea9e1e0c4f3270fbef6e7569dfff0fcc791d3b
Arman777777 <arman.manukyan4.y@gmail.com>
2022-11-15 11:27:49 +0400

    imports

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Login } from "./Login";
 export { default as Home } from "./Home";
 export { default as Navbar } from "./Navbar";
 export { default as Chats } from "./Chats";
+export { default as Message } from "./Message";
+export { default as SendMessage } from "./SendMessage";
